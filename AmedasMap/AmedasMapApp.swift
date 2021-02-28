17d48422a0aab01d38e5511a9c91e3c009b4acb8
Tomohiro Tashiro <tashiro@wni.com>
2021-02-28 18:24:03 +0900

    Initial Commit

---
+++ AmedasMap/AmedasMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmedasMapApp.swift
+//  AmedasMap
+//
+//  Created by tasshy on 2021/02/28.
+//
+
+import SwiftUI
+
+@main
+struct AmedasMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
