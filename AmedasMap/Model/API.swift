d561adb87150dfb52ea83ffb2777f19cdc708b5b
Tomohiro Tashiro <tashiro@wni.com>
2021-04-29 13:43:13 +0900

    fix API

--- AmedasMap/Model/API.swift
+++ AmedasMap/Model/API.swift
@@ -5,7 +5,7 @@
 //  Created by tasshy on 2021/03/01.
 //
 
-struct API {
+enum API {
     static let amedasPointTable = "https://www.jma.go.jp/bosai/amedas/const/amedastable.json"  // アメダス 地点リスト
     static let amedasLatestTime = "https://www.jma.go.jp/bosai/amedas/data/latest_time.txt"    // アメダス 最新データの時刻
     static let amedasMapData    = "https://www.jma.go.jp/bosai/amedas/data/map/%@00.json"      // アメダス 指定時刻の観測値
