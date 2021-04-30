4bb314fc11de68b3f72d68060bc12942168056a5
Tomohiro Tashiro <tashiro@wni.com>
2021-04-30 21:10:46 +0900

    Coordinator を MapView の下に。

--- AmedasMap/MapView.swift
+++ AmedasMap/MapView.swift
@@ -35,59 +35,58 @@ struct MapView: UIViewRepresentable {
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-}
 
-// MARK: - Coordinator
-// https://www.hackingwithswift.com/forums/swiftui/update-mapview-with-current-location-on-swiftui/1074
-// https://github.com/vvvegesna/MeetupReminder
-final class Coordinator: NSObject, MKMapViewDelegate {
-    private let parent: MapView
-    private var annotations: [MKAnnotation] = []
-
-    init(_ parent: MapView) {
-        self.parent = parent
-        super.init()
-    }
-    
-    func registerAnnotationViews() {
-        for identifier in AmedasData.allIdentifiers {
-            parent.mapView.register(AmedasAnnotationView.self, forAnnotationViewWithReuseIdentifier: identifier)
-        }
-    }
-
-    func updateAnnotations(viewModel: AmedasMapViewModel) {
-        parent.mapView.removeAnnotations(annotations)
-        annotations.removeAll()
+    // MARK: - Coordinator
+    // https://www.hackingwithswift.com/forums/swiftui/update-mapview-with-current-location-on-swiftui/1074
+    // https://github.com/vvvegesna/MeetupReminder
+    final class Coordinator: NSObject, MKMapViewDelegate {
+        private let parent: MapView
+        private var annotations: [MKAnnotation] = []
         
-        for data in viewModel.amedasData {
-            if let point = viewModel.amedasPoints[data.pointID], data.hasValidData(for: viewModel.displayElement) {
-                annotations.append(AmedasAnnotation(point: point, data: data, element: viewModel.displayElement))
+        init(_ parent: MapView) {
+            self.parent = parent
+            super.init()
+        }
+        
+        func registerAnnotationViews() {
+            for identifier in AmedasData.allIdentifiers {
+                parent.mapView.register(AmedasAnnotationView.self, forAnnotationViewWithReuseIdentifier: identifier)
             }
         }
-        LOG(#function + ", \(viewModel.dateText), \(viewModel.displayElement), plot \(annotations.count) points.")
-        parent.mapView.addAnnotations(annotations)
-        parent.mapView.setNeedsDisplay()
-    }
-
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        //LOG(#function)
-        guard let amedas = annotation as? AmedasAnnotation,
-              let reuseIdentifier = amedas.amedasData.reuseIdentifier(for: parent.viewModel.displayElement),
-              let view = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier, for: annotation) as? AmedasAnnotationView else { return nil }
-
-        view.point = amedas.point
-        return view
-    }
-    
-    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-        guard let amedasView = view as? AmedasAnnotationView,
-              let point = amedasView.point else { return }
-        LOG(#function + ", \(point.pointNameJa)")
-    }
-    
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        guard let amedasView = view as? AmedasAnnotationView,
-              let point = amedasView.point else { return }
-        LOG(#function + ", \(point.pointNameJa)")
+        
+        func updateAnnotations(viewModel: AmedasMapViewModel) {
+            parent.mapView.removeAnnotations(annotations)
+            annotations.removeAll()
+            
+            for data in viewModel.amedasData {
+                if let point = viewModel.amedasPoints[data.pointID], data.hasValidData(for: parent.viewModel.displayElement) {
+                    annotations.append(AmedasAnnotation(point: point, data: data, element: parent.viewModel.displayElement))
+                }
+            }
+            LOG(#function + ", \(viewModel.dateText), \(parent.viewModel.displayElement), plot \(annotations.count) points.")
+            parent.mapView.addAnnotations(annotations)
+            parent.mapView.setNeedsDisplay()
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            guard let amedas = annotation as? AmedasAnnotation,
+                  let reuseIdentifier = amedas.amedasData.reuseIdentifier(for: parent.viewModel.displayElement),
+                  let view = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier, for: annotation) as? AmedasAnnotationView else { return nil }
+            
+            view.point = amedas.point
+            return view
+        }
+        
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+            guard let amedasView = view as? AmedasAnnotationView,
+                  let point = amedasView.point else { return }
+            LOG(#function + ", \(point.pointNameJa)")
+        }
+        
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+            guard let amedasView = view as? AmedasAnnotationView,
+                  let point = amedasView.point else { return }
+            LOG(#function + ", \(point.pointNameJa)")
+        }
     }
 }
