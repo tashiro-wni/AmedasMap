c06931a604944fb3bf213e64d8ccb0cfd88200c7
Tomohiro Tashiro <tashiro@wni.com>
2022-06-05 15:49:10 +0900

    過去24時間の表を整形

--- AmedasMap/View/PointView.swift
+++ AmedasMap/View/PointView.swift
@@ -10,14 +10,6 @@ import SwiftUI
 struct PointView: View {
     @StateObject var viewModel: AmedasMapViewModel
 
-    private let dateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "H:mm"
-        dateFormatter.locale = .posix
-        dateFormatter.timeZone = .jst
-        return dateFormatter
-    }()
-
     private var pointName: String {
         String(format: "%@(%@)",
                viewModel.amedasPoints[viewModel.selectedPoint]?.pointNameJa ?? "",
@@ -31,34 +23,76 @@ struct PointView: View {
                     .padding(12)
                 // 選択地点のデータを時刻の新しい順に取り出し、正時(00分)のデータを24個取り出す
                 List(viewModel.selectedPointData.reversed().filter{ $0.is0min }.prefix(24), id: \.self) { item in
-                    Text(formattedText(item, width: geometry.size.width))
-                        .lineLimit(1)
-                    
+                    //Text(formattedText(item, width: geometry.size.width))
+                    //    .lineLimit(1)
+                    Colmun(item: item, width: geometry.size.width)
                 }
                 .listStyle(.plain)
                 .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             }
         }
     }
+}
     
-    private func formattedText(_ item: AmedasData, width: CGFloat) -> String {
-        var texts: [String] = []
-        texts.append(dateFormatter.string(from: Date(timeIntervalSince1970: item.time)))
-        if item.hasValidData(for: .temperature) {
-            texts.append("🌡" + item.text(for: .temperature))
+struct Colmun: View {
+    let item: AmedasData
+    let width: CGFloat
+    
+    private let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "H:mm"
+        dateFormatter.locale = .posix
+        dateFormatter.timeZone = .jst
+        return dateFormatter
+    }()
+    
+    var body: some View {
+        HStack {
+            Text(dateFormatter.string(from: Date(timeIntervalSince1970: item.time)))
+                .frame(width: 50, alignment: .trailing)
+            
+            if item.hasValidData(for: .temperature) {
+                Text(item.text(for: .temperature))
+                    .frame(width: 70, alignment: .trailing)
+            }
+            if item.text(for: .precipitation) != item.invalidText {
+                Text(item.text(for: .precipitation))
+                    .frame(width: 90, alignment: .trailing)
+            }
+            if item.hasValidData(for: .wind) {
+                Text(item.text(for: .wind))
+                    .frame(width: 120, alignment: .trailing)
+            }
+            if width > 500, item.hasValidData(for: .sun) {
+                Text(item.text(for: .sun))
+                    .frame(width: 60, alignment: .trailing)
+            }
+            if width > 500, item.hasValidData(for: .humidity) {
+                Text(item.text(for: .humidity))
+                    .frame(width: 50, alignment: .trailing)
+            }
         }
-        if item.text(for: .precipitation) != item.invalidText {
-            texts.append("☂️" + item.text(for: .precipitation))
-        }
-        if item.hasValidData(for: .wind) {
-            texts.append("🎐" + item.text(for: .wind))
-        }
-        if width > 500, item.hasValidData(for: .sun) {
-            texts.append("☀️" + item.text(for: .sun))
-        }
-        if width > 500, item.hasValidData(for: .humidity) {
-            texts.append("💦" + item.text(for: .humidity))
-        }
-        return texts.joined(separator: ", ")
     }
 }
+    
+//    private func formattedText(_ item: AmedasData, width: CGFloat) -> String {
+//        var texts: [String] = []
+//        texts.append(dateFormatter.string(from: Date(timeIntervalSince1970: item.time)))
+//        if item.hasValidData(for: .temperature) {
+//            texts.append("🌡" + item.text(for: .temperature))
+//        }
+//        if item.text(for: .precipitation) != item.invalidText {
+//            texts.append("☂️" + item.text(for: .precipitation))
+//        }
+//        if item.hasValidData(for: .wind) {
+//            texts.append("🎐" + item.text(for: .wind))
+//        }
+//        if width > 500, item.hasValidData(for: .sun) {
+//            texts.append("☀️" + item.text(for: .sun))
+//        }
+//        if width > 500, item.hasValidData(for: .humidity) {
+//            texts.append("💦" + item.text(for: .humidity))
+//        }
+//        return texts.joined(separator: ", ")
+//    }
+
