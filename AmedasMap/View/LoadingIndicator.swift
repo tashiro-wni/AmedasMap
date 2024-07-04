49493b194f8086a063ee750a3b681609b5dedcc8
tasshy <tasshy@gmail.com>
2024-07-04 23:59:01 +0900

    change LoadingIndicator backgroundColor in dark mode

--- AmedasMap/View/LoadingIndicator.swift
+++ AmedasMap/View/LoadingIndicator.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct LoadingIndicator: View {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+
     var body: some View {
         ProgressView()
             .progressViewStyle(.circular)
             .padding()
             .tint(.white)
-            .background(Color.black.opacity(0.5))
+            .background(colorScheme == .dark ? Color.gray.opacity(0.5) : Color.black.opacity(0.5))
             .cornerRadius(8)
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
     }
